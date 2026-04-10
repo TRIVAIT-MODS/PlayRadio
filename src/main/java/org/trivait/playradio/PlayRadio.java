04d26d7f1d2b524e2f294966a74c84e9c1edad10
TRIVAIT <dimitmapper@gmail.com>
2026-04-10 09:53:51 +0400

    initial commit

---
+++ src/main/java/org/trivait/playradio/PlayRadio.java
@@ -0,0 +1,15 @@
+package org.trivait.playradio;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PlayRadio implements ClientModInitializer {
+    public static final String MOD_ID = "playradio";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
